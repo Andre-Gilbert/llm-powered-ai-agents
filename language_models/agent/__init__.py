fdee3cd7bb6fb66851632af18437c4f1d0d9ff5b
Andre Gilbert <andre.gilbert.77110@gmail.com>
2024-08-21 08:31:55 +0200

    added content

--- language_models/agent/__init__.py
+++ language_models/agent/__init__.py
@@ -1,6 +1,10 @@
 from language_models.agent.agent import Agent
 from language_models.agent.chat import Step
-from language_models.agent.output_parser import OutputType, PromptingStrategy
+from language_models.agent.output_parser import (
+    OutputType,
+    PromptingStrategy,
+    get_schema_from_args,
+)
 from language_models.agent.workflow import (
     Workflow,
     WorkflowFunctionStep,
